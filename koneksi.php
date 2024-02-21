0ef311b08173f57b1754b557c8821f043f433fe3
whyurmdhn29 <whyurmdhn349@gmail.com>
2024-02-21 16:09:43 +0700

    ujikom hari pertama

---
+++ koneksi.php
@@ -0,0 +1,6 @@
+<?php
+include "koneksi.php";
+session_start();
+if(!isset($_SESSION)['user']){    
+}
+?>
---
+++ logout.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+session_destroyer();{
+
+header(location:'login.php');
+}
+
+?>
